5d7204e7c29d301f2f7cee5b77d827830da4376e
Aleksandra <s-bass11@yandex.ru>
2022-04-29 00:28:35 +0300

    Zadacha5

---
+++ Zadacha5/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.WriteLine("Введите трехзначное число: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a > 99 && a < 1000)
+{
+int digit2 = (a / 10) % 10;
+Console.Write("Вторая цифра трехзначного числа: ");
+Console.Write(digit2);
+}
+else
+{
+    Console.Write("Число не является трехзначным");
+}
