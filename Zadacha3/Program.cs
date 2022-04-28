77ca0a65e60ca613078b15801ae943c669ab3ce4
Aleksandra <s-bass11@yandex.ru>
2022-04-29 00:27:38 +0300

    Zadacha3

---
+++ Zadacha3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Выяснить является ли число чётным
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int b = a % 2;
+if (b == 0)
+{
+    Console.Write("Число четное");
+}
+else
+{
+    Console.Write("Число нечетное");
+}
