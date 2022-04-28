ee94156413f462289e3545c26959473e48c8b713
Aleksandra <s-bass11@yandex.ru>
2022-04-29 00:29:26 +0300

    Zadacha7

---
+++ Zadacha7/Program.cs
@@ -0,0 +1,8 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+Console.WriteLine("ВВедите трехзначное число: ");
+int x = int.Parse(Console.ReadLine());
+int digit1 = x / 100;
+int digit2  = (x / 10) % 10;
+int digit3 = x % 10;
+Console.Write("Ответ на задачу: " + digit1 + digit3);
