d43b4b320d849baabf745205e7271a01619ffd6c
Aleksandra <s-bass11@yandex.ru>
2022-04-29 00:26:03 +0300

    Zadacha1

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,15 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+
+Console.Write("Введите число 1: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int number2 = int.Parse(Console.ReadLine());
+int square = number2 * number2;
+if (number1 == square)
+{
+    Console.Write("Число1 является квадратом числа2");
+}
+else
+{
+    Console.Write("Число1 не является квадратом числа2");
+}
