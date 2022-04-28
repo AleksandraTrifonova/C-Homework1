524a124d3385d47f252c77a015666ebcc3d9f920
Aleksandra <s-bass11@yandex.ru>
2022-04-29 00:28:05 +0300

    Zadacha4

---
+++ Zadacha4/Program.cs
@@ -0,0 +1,11 @@
+﻿//Показать четные числа от 1 до N
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int n2 = 2;
+
+while (n2 <= n)
+{
+    Console.Write(" ");
+    Console.Write(n2);
+    n2 = n2 + 2;
+}
