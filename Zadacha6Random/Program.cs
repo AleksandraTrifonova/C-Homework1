fb3e93d5882b41b3b6c8b55f3d45345f4dfbd17f
Aleksandra <s-bass11@yandex.ru>
2022-05-16 00:32:33 +0300

    Zadacha6Random

---
+++ Zadacha6Random/Program.cs
@@ -0,0 +1,16 @@
+﻿//Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int x = new Random().Next(10,100);
+Console.WriteLine(x);
+int digit1 = (x/ 10) % 10;
+int digit2 = x % 10;
+if (digit1 > digit2)
+{
+Console.Write("Наибольшая цифра числа: ");
+Console.Write(digit1);
+}
+else
+{
+Console.Write("Наибольшая цифра числа: ");
+Console.Write(digit2);    
+}
